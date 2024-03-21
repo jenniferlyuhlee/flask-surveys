6f78104378849c0e0f3d0541b772cbcb553905bf
jenniferlyuhlee <jenniferlyuhlee@gmail.com>
2024-03-21 00:16:29 -0700

    added session

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
+		
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-
-responses = []
 survey = satisfaction_survey
 
 @app.route('/')
@@ -22,11 +22,19 @@ def show_survey():
     title = title, 
     instructions = instructions)
 
+@app.route('/start', methods=["POST"])
+def handle_start():
+    """Post request that clears session data to change state"""
+
+    session['responses'] = []
+    return redirect('questions/0')
+
 
 @app.route('/questions/<int:question_num>')
 def show_question(question_num):
     """Shows question form based on how far the user has answered"""
 
+    responses = session['responses']
     #prevents user from going back to questions when all have been answered
     if len(responses) == len(survey.questions):
         return redirect('/completed')  
@@ -50,8 +58,12 @@ def show_question(question_num):
 def handle_answer():
     """Adds answer to responses list and redirects users to the next correct page"""
 
+    #answer is added to session data
     answer = request.form.get('answer')
+    responses = session['responses']
     responses.append(answer)
+    #set session to the updated list of answers
+    session['responses'] = responses
 
     #if all questions have been answered redirect to completion confirmation page
     if len(responses) == len(survey.questions):
